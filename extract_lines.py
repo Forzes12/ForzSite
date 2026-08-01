2974defd950f4388cd5a7b4d0611d885520e2fd1
Forzes12 <axisstream28@gmail.com>
2026-08-01 14:38:49 +0300

    Delete extract_lines.py

--- extract_lines.py
+++
@@ -1,6 +0,0 @@
-with open('index.html', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-# Print lines 8940 to 8980 (showAuthModal function)
-for i in range(8939, min(8980, len(lines))):
-    print(f"{i+1}: {lines[i]}", end='')
